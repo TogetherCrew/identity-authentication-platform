a4bbea73029e719dca287aaba33766c3d6e39530
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2024-06-28 11:47:53 +0400

    feat: add cors and helmet

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
 import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.use(helmet());
+  app.enableCors();
   app.useLogger(app.get(Logger));
   app.useGlobalInterceptors(new LoggerErrorInterceptor());
   const configService = app.get(ConfigService);
