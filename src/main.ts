bd56a13db976a6dd0f89d05be65365f4d9a98499
Behzad Rabiei <behzad.rabiei.77@gmail.com>
2024-07-03 13:40:39 +0400

    chore: add compression

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import helmet from 'helmet';
+import * as compression from 'compression';
 import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
 import { setupSwagger } from './doc';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
   app.use(helmet());
+  app.use(compression());
   app.enableCors();
   app.useLogger(app.get(Logger));
   app.useGlobalInterceptors(new LoggerErrorInterceptor());
