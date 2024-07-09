e7e7d26055310ed0717221ffb74728062842649f
Behzad Rabiei <behzad.rabiei.77@gmail.com>
2024-07-09 21:54:19 +0400

    test: add auth.service tests

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,34 @@
+// test/auth/auth.service.spec.ts
 import { Test, TestingModule } from '@nestjs/testing';
-import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { AuthService } from '../../src/auth/auth.service';
+import { ConfigService } from '@nestjs/config';
+import * as moment from 'moment';
+import { JwtPayload } from './types/jwt-payload.type';
+import * as jwt from 'jsonwebtoken';
 
 describe('AuthService', () => {
   let service: AuthService;
+  const mockPublicKey = '0xabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdef';
+  const mockJwtSecret = 'jwtSecret';
+  const mockJwtExpiresIn = '60';
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        JwtService,
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn((key: string) => {
+              if (key === 'wallet.publicKey') return mockPublicKey;
+              if (key === 'jwt.secret') return mockJwtSecret;
+              if (key === 'jwt.expiresIn') return mockJwtExpiresIn;
+            }),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
@@ -14,5 +36,40 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('signPayload', () => {
+    it('should return a valid JWT', async () => {
+      const payload: JwtPayload = {
+        sub: '1',
+        provider: 'google',
+        iat: moment().unix(),
+        exp: moment().add(mockJwtExpiresIn, 'minutes').unix(),
+        iss: mockPublicKey,
+      };
+      const token = await service.signPayload(payload);
+      expect(typeof token).toBe('string');
+      const decoded = jwt.verify(token, mockJwtSecret, { algorithms: ['HS256'] });
+      expect(decoded).toMatchObject(payload);
+    });
+  });
+
+  describe('validateToken', () => {
+    it('should validate a token correctly', async () => {
+      const payload: JwtPayload = {
+        sub: '1',
+        provider: 'google',
+        iat: moment().unix(),
+        exp: moment().add(mockJwtExpiresIn, 'minutes').unix(),
+        iss: mockPublicKey,
+      };
+      const token = jwt.sign(payload, mockJwtSecret, { algorithm: 'HS256' });
+      const decoded = await service.validateToken(token);
+      expect(decoded).toMatchObject(payload);
+    });
+
+    it('should return null if token is invalid', async () => {
+      await expect(service.validateToken('invalid.token.here')).rejects.toThrow(jwt.JsonWebTokenError);
+    });
   });
 });
