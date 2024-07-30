cb1d893d91927972d8038d2530c3ab6a06865b76
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2024-07-30 22:27:22 +0400

    chore: add http-exception filter

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import { Logger, LoggerErrorInterceptor } from 'nestjs-pino'
 import * as session from 'express-session'
 import { VersioningType, ValidationPipe } from '@nestjs/common'
 import { setupSwagger } from './doc'
+import { HttpExceptionFilter } from './filters/http-exception.filter'
+
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, { bufferLogs: true })
     app.useGlobalPipes(new ValidationPipe())
@@ -15,6 +17,7 @@ async function bootstrap() {
     app.enableCors()
     app.useLogger(app.get(Logger))
     app.useGlobalInterceptors(new LoggerErrorInterceptor())
+    app.useGlobalFilters(new HttpExceptionFilter())
     const configService = app.get(ConfigService)
     const port = configService.get('app.port')
     app.use(
@@ -37,4 +40,5 @@ async function bootstrap() {
     setupSwagger(app)
     await app.listen(port)
 }
+
 bootstrap()
