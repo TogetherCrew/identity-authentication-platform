9aea2d54333c4a0ee37744c9ebcf0c7f1f883986
Behzad Rabiei <behzad.rabiei.77@gmail.com>
2024-11-26 13:51:39 +0200

    fix: define the idToChain output type

--- src/utils/viem.utils.service.ts
+++ src/utils/viem.utils.service.ts
@@ -3,6 +3,8 @@ import { SUPPORTED_CHAINS } from '../shared/constants/chain.constants'
 import { SupportedChainId } from '../shared/types/chain.type'
 import { createPublicClient, http, Client } from 'viem'
 import * as chains from 'viem/chains'
+import { Chain } from 'viem/chains'
+
 @Injectable()
 export class ViemUtilsService {
     private publicClients: Map<number, any>
@@ -29,7 +31,7 @@ export class ViemUtilsService {
         return this.publicClients.get(chainId)
     }
 
-    idToChain(chainId: SupportedChainId) {
+    idToChain(chainId: SupportedChainId): Chain {
         for (const chain of Object.values(chains)) {
             if ('id' in chain) {
                 if (chain.id === chainId) {
